917dd3b1dcb22656b6a0aaf985f4f3b7c48d6f43
Jayant <jayantmehra02@gmail.com>
2021-08-28 14:11:51 +0530

    project completed

---
+++ src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import Chart from '../../components/chart/Chart';
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import './home.css';
+import { userData } from '../../dummyData';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
